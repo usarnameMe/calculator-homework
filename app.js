42cd47274c31cff42f08211147c42e5059f5f9b5
Gvn <gvanca.uplisashvili@gmail.com>
2023-02-06 14:45:06 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,40 @@
-const screen = document.getElementById("screen");
-const clearAll = document.getElementById("clearAll");
-const clear = document.getElementById("clear");
-const equal = document.getElementById("equal");
-const number = document.getElementsByClassName("number");
-const operations = document.getElementsByClassName("dif-color");
+const screen = document.querySelectorAll("#screen");
+const clearAll = document.querySelectorAll("#clearAll");
+const clear = document.querySelectorAll("#clear");
+const equal = document.querySelectorAll("#equal");
+const number = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".dif-color");
+
+clear.addEventListener("click", input.value.slice(0, -1));
+clearAll.addEventListener("click", (input.value = ""));
+
+let operator = "";
+let num1;
 
 for (let i = 0; i < number.length; i++) {
   number[i].addEventListener("click", function () {
     screen.value += this.textContent;
   });
 }
-
-for (let i = 0; i < operations.length; i++) {
-  operations[i].addEventListener("click", function () {
-    if (this.textContent === "+") {
-      screen.value = "";
-    } else if (this.textContent === "-") {
-      screen.value = "";
-    } else if (this.textContent === "*") {
-      screen.value = "";
-    } else if (this.textContent === "/") {
-      screen.value = "";
-    } else if (this.textContent === "AC") {
-      screen.value = "";
-    } else if (this.textContent === "C") {
-      screen.value = screen.value.slice(0, -1);
-    }
+for (let i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", function () {
+    operator = operators[i].innerHTML;
+    num1 = Number(input.value);
+    screen.value = "";
   });
 }
 
-let operator = "";
-let num1;
 equal.addEventListener("click", function () {
-  let num2 = parseFloat(screen.value);
+  let num2 = Number(screen.value);
   let result;
-
-  if (operator === "+") {
-    result = num1 + num2;
-  } else if (operator === "-") {
-    result = num1 - num2;
-  } else if (operator === "*") {
-    result = num1 * num2;
-  } else if (operator === "/") {
-    result = num1 / num2;
-  }
-
+  operator === "+"
+    ? num1 + num2
+    : operator === "-"
+    ? num1 - num2
+    : operator === "*"
+    ? num1 * num2
+    : operator === "/"
+    ? num1 / num2
+    : "";
   screen.value = result;
 });
